8ddea470d01e47546bb5d632a1cbca88aa46f010
Evgeny Luvsandugar <evgeny.luvsandugar@fundraiseup.com>
2023-09-16 23:58:43 +0400

    Try to switch to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,15 +29,15 @@ const config = {
 
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
